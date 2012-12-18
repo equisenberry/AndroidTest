31ce33ea600999349655c2ca5f5186227b2de459
Evan Quisenberry <evan.quisenberry@compuware.com>
2012-12-18 15:27:25 -0500

    Lint stuff

---
+++ AndroidTest/src/com/example/androidtest/TestActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_activity_layout);
+	}
+}
